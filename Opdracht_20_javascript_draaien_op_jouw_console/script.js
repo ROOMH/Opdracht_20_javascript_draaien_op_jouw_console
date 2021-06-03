8bc843bcf0c1661dd0a30e95eea22c4e57f3ef92
Romy <80348985+ROOMH@users.noreply.github.com>
2021-06-03 15:16:43 +0200

    Add files via upload

---
+++ Opdracht_20_javascript_draaien_op_jouw_console/script.js
@@ -0,0 +1,10 @@
+console.log("Hallo Winc Academy studenten")
+
+// Wanneer je een comment toevoegd aan de code zul je dit niet zien in je console omdat je dit niet console.lo doet naar je node. 
+
+
+/* let sum= 1230941 * 1823794;
+console.log(sum);
+
+let division = (637263 / 54);
+console.log(division);*/
